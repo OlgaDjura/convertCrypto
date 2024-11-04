9adf65ac5049c03383840349c7ae17ff7d0ea5d4
Olga <olgajura@mail.ru>
2024-11-04 21:19:44 +0700

    'converter_crypto'

---
+++ crypto-exchanger/src/components/Input.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+        changeAmount: {
+            type: Function,
+            required: true
+        },
+        convert: {
+            type: Function,
+            required: true
+        }
+    }
+}
+</script>
+<template> <div>
+<input min="0" type="number" @input="changeAmount($event.target.value)" placeholder="Введите число"> <br>
+<button @click="convert()">Конвертировать</button>
+</div>
+</template>
+
+
+<style scoped>
+input {
+    outline: none;
+    border-radius: 10px;
+    border: none;
+    padding: 10px 15px;
+    background-color: rgb(55, 99, 55);
+    color: rgb(252, 250, 250);
+    font-size: 30px;
+}
+
+button {
+    margin-top: 20px;
+    border-radius: 20px;
+    background-color: rgb(91, 133, 91) ;
+    height: 50px;
+    color: aliceblue;
+    cursor: pointer;
+}
+
+</style>
---
+++ crypto-exchanger/src/components/Selector.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+    props: {
+        setCrypto: {
+            type: Function,
+            required: true
+        }
+    },
+    data() {
+        return {
+        current: ''
+        }
+    },
+    methods: {
+        selectItem(val) {
+            this.setCrypto(val);
+            this.current = val;
+        }
+    },
+
+}
+</script>
+<template>
+<ul>
+    <li @click="selectItem('BTC')" :className="current == 'BTC' ? 'active' : ''">Bitcoin</li>
+    <li @click="selectItem('ETH')" :className="current == 'ETH' ? 'active' : ''">ETH</li>
+    <li @click="selectItem('USDT')" :className="current == 'USDT' ? 'active' : ''">USDT</li>
+</ul>
+</template>
+
+
+<style scoped>
+ul {
+    list-style: none;
+    width: 200px;
+    padding: 3px;
+    border: 3px solid rgb(39, 84, 39);
+    border-radius: 15px;
+}
+
+li {
+    display: block;
+    background-color: rgb(24, 56, 26);
+    color: aliceblue;
+    padding: 20px 0;
+}
+li:hover, li.active {
+    background-color: rgb(2, 54, 37);
+    cursor: pointer;
+}
+
+
+</style>
